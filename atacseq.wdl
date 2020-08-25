a7f26f3be2ca25d4be6aa8bc1d9a845909e4f386
Bekir Erguener <berguener@cemm.at>
2020-08-25 16:38:15 +0200

    Completed alignment task

--- atacseq.wdl
+++ atacseq.wdl
@@ -1,28 +1,74 @@
 version 1.0
 
-task process_atacseq_sample {
+struct Sample {
+    String sample_name
+    String read_type
+    String genome
+    Int genome_size
+    String raw_bams
+    Int raw_size_mb
+}
+
+
+task test_task {
     input{
         String output_dir
         String sample_name
-        String sample_dir = "~{output_dir}/~{sample_name}"
-        String sample_raws
-        String bowtie2_ref
-
-        Int threads = 4
     }
+    String sample_dir = "~{output_dir}/~{sample_name}"
+    File sample_tsv  = "~{output_dir}/config_files/~{sample_name}.tsv"
+    Map[String, String] sample_map = read_map(sample_tsv)
+    String sample_raws = sample_map['raw_bams']
+
+    command{
+        echo "~{sample_dir}"
+        echo "~{sample_raws}"
+    }
+    output{
+        String my_out = stdout()
+    }
+}
+
+task bowtie2_align {
+    input{
+        String output_dir
+        String config_dir
+        String sample_name
+        String bowtie2_index
+        String? adapter_fasta
+
+        Int cpus = 8
+    }
+
+    String sample_dir = "~{output_dir}/~{sample_name}"
+    File sample_tsv  = "~{config_dir}/~{sample_name}.tsv"
+    Map[String, String] sample_map = read_map(sample_tsv)
+    String raw_bams = sample_map['raw_bams']
+    Int raw_size_mb = sample_map['raw_size_mb']
+    Int memory = 6000 + raw_size_mb
+    String interleaved_in = if(sample_map['read_type'] == 'paired') then '--interleaved_in' else ' '
+    String interleaved = if(sample_map['read_type'] == 'paired') then '--interleaved' else ' '
+
     command {
-        mkdir -p ~{output_dir};
-        mkdir -p ~{sample_dir};
-        mkdir -p "~{sample_dir}/tmp";
+        [ ! -d "~{output_dir}" ] && mkdir -p ~{output_dir};
+        [ ! -d "~{sample_dir}" ] && mkdir -p ~{sample_dir};
 
-        for i in "~{sample_raws}"; do samtools fastq $i 2> "~{sample_dir}/samtools_fastq.log" ; done | \
-            fastp --stdin --interleaved_in --stdout 2> "~{sample_dir}/~{sample_name}_fastp.log" | \
-            bowtie2 --very-sensitive --no-discordant -p ~{threads} --maxins 2000 -x ~{bowtie2_ref} --met-file "~{sample_dir}/~{sample_name}_bowtie2.met" --interleaved - 2> "~{sample_dir}/~{sample_name}_bowtie.log" | \
-            samtools fixmate -O SAM - - 2> "~{sample_dir}/~{sample_name}_fixmate.log" | \
-            samblaster 2> "~{sample_dir}/~{sample_name}_samblaster.log" | \
-            samtools sort -o "~{sample_dir}/~{sample_name}.bam" - 2> "~{sample_dir}/~{sample_name}_samtools_sort.log";
-            samtools index "~{sample_dir}/~{sample_name}.bam" 2> "~{sample_dir}/~{sample_name}_samtools_index.log";
+        for i in ~{raw_bams}; do samtools fastq $i 2>> "~{sample_dir}/~{sample_name}.samtools.log" ; done | \
+            fastp --stdin ~{interleaved_in} --stdout --html "~{sample_dir}/~{sample_name}.fastp.html" --json "~{sample_dir}/~{sample_name}.fastp.json" 2> "~{sample_dir}/~{sample_name}.fastp.log" | \
+            bowtie2 --very-sensitive --no-discordant -p ~{cpus} --maxins 2000 -x ~{bowtie2_index} --met-file "~{sample_dir}/~{sample_name}.bowtie2.met" ~{interleaved} - 2> "~{sample_dir}/~{sample_name}.txt" | \
+            samblaster --addMateTags 2> "~{sample_dir}/~{sample_name}.samblaster.log" | \
+            samtools sort -o "~{sample_dir}/~{sample_name}.bam" - 2>> "~{sample_dir}/~{sample_name}.samtools.log";
+            samtools index "~{sample_dir}/~{sample_name}.bam" 2>> "~{sample_dir}/~{sample_name}.samtools.log";
+            samtools flagstat "~{sample_dir}/~{sample_name}.bam" > "~{sample_dir}/~{sample_name}.samtools_flagstat.log";
     }
+
+    runtime {
+        rt_cpus: cpus
+        rt_mem: memory
+        rt_queue: "mediumq"
+        rt_time: "2-00:00:00"
+    }
+
     output {
         File output_bam = "~{sample_dir}/~{sample_name}.bam"
         File output_bai = "~{sample_dir}/~{sample_name}.bam.bai"
@@ -31,20 +77,30 @@ task process_atacseq_sample {
 
 workflow atacseq {
     input{
-        String output_dir
-        Array[Map[String, String]] sample_list
-        Map[String, String] tools
-        String bowtie2_ref
+        String project_path
+        String project_name
+        String genome
+        String regulatory_regions
+        String blacklisted_regions
+        String chromosome_sizes
+        String unique_tss
+        Array[String] sample_list
+        String bowtie2_index
+        String adapter_fasta
     }
+
+    String output_dir = "~{project_path}/atacseq_results"
+    String config_dir = "~{project_path}/config_files"
 
     scatter(sample in sample_list) {
-        call process_atacseq_sample {
+
+        call bowtie2_align {
             input:
-                output_dir = "~{output_dir}",
-                sample_name = "~{sample[sample_name]}",
-                sample_raws = "~{sample[sample_raws]}",
-                bowtie2_ref = "~{bowtie2_ref}"
+                output_dir = output_dir,
+                config_dir = config_dir,
+                sample_name = sample,
+                bowtie2_index = bowtie2_index,
+                adapter_fasta = adapter_fasta
         }
     }
-
 }
---
+++ configurator.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+import argparse
+import yaml, json
+import csv
+import os
+import sys
+from collections.abc import Mapping
+from string import Template
+
+def update(d, u):
+    """
+    Recursively updates the entries in a given dictionary
+    :param d: The dictionary to be updated
+    :param u: The values which will be added to the input dictionary
+    :return: Updated dictionary
+    """
+    for k, v in u.items():
+        if isinstance(v, Mapping):
+            d[k] = update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="JSON config generator for the ATAC-seq pipeline")
+    parser.add_argument('--project-config', '-c',
+                        dest='project_config',
+                        help='Project specific .yaml config file',
+                        type=str)
+    parser.add_argument('--pipeline-config', '-p',
+                        dest='pipeline_config',
+                        help='Pipeline specific .yaml config file',
+                        type=str)
+    args = parser.parse_args()
+
+    # Read in config files
+    # default will contain pipeline configurations
+    default = {}
+    # specific will contain the project configurations
+    specific = {}
+    with open(args.pipeline_config, 'r') as stream:
+        try:
+            default = yaml.safe_load(stream)
+        except yaml.YAMLError as exception:
+            sys.stderr.write(str(exception))
+    with open(args.project_config, 'r') as stream:
+        try:
+            specific = yaml.safe_load(stream)
+        except yaml.YAMLError as exception:
+            sys.stderr.write(str(exception))
+    update(default, specific)
+    # rename the updated dictionary
+    config = default
+
+    # Create the
+    project_path = config['project_path']
+    json_path = os.path.join(project_path, 'config_files')
+    if not os.path.exists(project_path):
+        os.mkdir(project_path)
+    if not os.path.exists(json_path):
+        os.mkdir(json_path)
+
+    project_genome = config['genome']
+    project_genome_size = config['genome_sizes'][project_genome]
+    inputs_dict = {'atacseq.project_name': config['project_name'],
+                   'atacseq.project_path': config['project_path'],
+                   'atacseq.genome': project_genome,
+                   'atacseq.adapter_fasta': config['adapter_fasta'],
+                   'atacseq.bowtie2_index': config['bowtie2_index'][project_genome],
+                   'atacseq.chromosome_sizes': config['chromosome_sizes'][project_genome],
+                   'atacseq.blacklisted_regions': config['blacklisted_regions'][project_genome],
+                   'atacseq.unique_tss': config['unique_tss'][project_genome],
+                   'atacseq.regulatory_regions': config['regulatory_regions'][project_genome]
+                   }
+
+    sas_file = config['sample_annotation']
+    sas_dict = {}
+    with open(sas_file, 'r') as sas:
+        reader = csv.DictReader(sas, dialect='excel')
+        for row in reader:
+            if 'sample_name' in row:
+                if row['sample_name'] in sas_dict:
+                    sas_dict[row['sample_name']].append(row)
+                else:
+                    sas_dict[row['sample_name']] = [row]
+
+    inputs_dict['atacseq.sample_list'] = list(sas_dict.keys())
+
+    project_json = os.path.join(json_path, '{}.inputs.json'.format(config['project_name']))
+    with open(project_json, 'w') as output:
+        json.dump(inputs_dict, output, indent=2)
+
+    sample_dicts = []
+    for sample in sas_dict:
+        sample_dict = {'sample_name': sample,
+                       'read_type': sas_dict[sample][0]['read_type'],
+                       'organism': sas_dict[sample][0]['organism'],
+                       'genome': project_genome,
+                       'genome_size': project_genome_size,
+                       'raw_bams': ''}
+
+        row_list = sas_dict[sample]
+        number_of_rows = len(row_list)
+        bam_sources = []
+        raw_size_mb = 0
+        for i in range(number_of_rows):
+            if 'data_source' in row_list[i] and row_list[i]['data_source'] != '':
+                source_template = config['data_sources'][row_list[i]['data_source']]
+                source = source_template.format(**row_list[i])
+                bam_sources.append(source)
+                if(os.path.exists(source)):
+                    source_stats = os.stat(source)
+                    raw_size_mb += int(source_stats.st_size / (1024 * 1024))
+                #sample_dict['raw_bams'].append(source)
+        sample_dict['raw_bams'] = ' '.join(bam_sources)
+        sample_dict['raw_size_mb'] = raw_size_mb
+        # sample_json = os.path.join(json_path, '{}.json'.format(sample))
+        # with open(sample_json, 'w') as output:
+        #     json.dump(sample_dict, output, indent=2)
+        sample_tsv = os.path.join(json_path, '{}.tsv'.format(sample))
+        with open(sample_tsv, 'w') as output:
+            for key in sample_dict:
+                output.write('{}\t{}\n'.format(key, sample_dict[key]))
